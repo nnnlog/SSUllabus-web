629e98fbc835529901543991a5b6ed0c81ef6430
nnnlog <sorisem4106@naver.com>
2024-03-24 00:18:02 +0900

    Update endpoint url

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,8 +1,8 @@
 import {GraphQLClient} from 'graphql-request'
 import {getSdk, SubjectsQueryVariables} from "../types/graphql";
 
-const api = `http://127.0.0.1:8000/graphql`;
-// const api = `${location.origin}/graphql`;
+const api = import.meta.env.DEV ? `http://127.0.0.1:8000/graphql` : `${location.origin}/graphql`;
+// const api = ;
 
 const client = new GraphQLClient(api);
 const sdk = getSdk(client);
